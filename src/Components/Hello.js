8c93188f1b2471472246e65e4c3554c64f958d34
Afaf KELAI <ea_kelai@esi.dz>
2021-04-21 10:12:07 +0100

    ok

---
+++ src/Components/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Hello extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                Hello World
+            </div>
+         );
+    }
+}
+ 
+export default Hello;
