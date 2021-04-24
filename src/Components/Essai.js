2d55dad4cb9d9d30e8c8bfc780fc86615ec55365
Afaf KELAI <ea_kelai@esi.dz>
2021-04-24 12:24:54 +0100

    updates

---
+++ src/Components/Essai.js
@@ -0,0 +1,11 @@
+import react from 'react'
+import { Button } from 'reactstrap';
+
+function Essai(){
+    return(
+        //return our JSX 
+
+        <Button>Click Me</Button>
+    )
+}
+export default Essai;
--- src/Components/Hello.js
+++ src/Components/Hello.js
@@ -1,12 +1,42 @@
+import { blue } from 'kleur';
 import React, { Component } from 'react';
+import './Hello.css'
+import styled from 'styled-components'
+import image from '../logo512.png'
+import logo from '../img/css.svg'
+import { width } from 'dom-helpers';
 
+
+            const Button = styled.button`
+            cursor: pointer;
+            border: 1px solid #1a202c;
+            padding: 8px;
+            min-width: 64px;
+
+            background: transparent;
+
+            transition: all 0.1s ease-in;
+
+            `;
 
 class Hello extends Component {
-    state = {  }
+// jsx 
+// je veux ajouter du js avec du jsx == {mon java scripte }
+ 
     render() { 
+   
+        const mystyle = {
+            color: "white",
+            backgroundColor: "DodgerBlue",
+            padding: "10px",
+            fontFamily: "Arial",
+            backgroundColor:"blue"
+          };
         return ( 
-            <div>
-                Hello World
+            <div >
+               <h1> Hello World</h1>
+               <img style={{height:"100px",width:"100px"}} src={logo} alt="logo"></img>
+               <Button>Click me</Button>
             </div>
          );
     }
